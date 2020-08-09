486fccaca4aeffd82e1e5077eb0e581a779e2459
Carlos Moraes <carlos.moraes@nuvem.net>
2020-08-09 10:48:04 -0400

    Feature: BottomNavigation + Pageheader

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import StudyTabs from './StudyTabs';
 import Landing from '../pages/Landing';
 import GiveClasses from '../pages/GiveClasses';
+
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -13,6 +15,7 @@ function AppStack() {
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Landing" component={Landing} />
         <Screen name="GiveClasses" component={GiveClasses} />
+        <Screen name="Study" component={StudyTabs} />
       </Navigator>
     </NavigationContainer>
   );
